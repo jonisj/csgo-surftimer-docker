e23d26ba28d83aab69126b544073b1a637bebb22
Joni Sjöstedt <joni.sjostedt@geniem.com>
2022-11-18 01:10:46 +0200

    Updated to SurfTimer 1.1.3.1 and fixed missing stripper files.

--- etc/functions/install_surftimer.sh
+++ etc/functions/install_surftimer.sh
@@ -1,5 +1,5 @@
 ## Install SurfTimer
-# https://github.com/surftimer/Surftimer-Official/releases
+# https://github.com/surftimer/Surftimer-Official
 function install_surftimer {
 	echo ">>>> Installing SurfTimer"
 	case "$(check_version "surftimer" "${SURFTIMER_VERSION}")" in
@@ -7,13 +7,13 @@ function install_surftimer {
 			echo ">> Installing SurfTimer ${SURFTIMER_VERSION}"
 			# Folder structure and files in releases changes every time,
 			# don't bother trying to make anything fancy here
-			dl_extract "zip" "surftimer" "https://github.com/surftimer/SurfTimer/releases/download/1.1.2/SurfTimer.1.1.2.911.838f2a2.SM1.11.zip"
+			dl_extract "zip" "surftimer" "https://github.com/surftimer/SurfTimer/releases/download/1.1.3.1/SurfTimer.1.1.3.1.912.01b3153.SM1.11.zip"
 
 			cp -R "$TEMPDIR/surftimer/addons/sourcemod/plugins/"* "${STEAMAPPDIR}/${STEAMAPP}/addons/sourcemod/plugins/"
 			cp -Rn "$TEMPDIR/surftimer/addons/sourcemod/configs/"* "${STEAMAPPDIR}/${STEAMAPP}/addons/sourcemod/configs/"
 			cp -R "$TEMPDIR/surftimer/addons/sourcemod/translations/"* "${STEAMAPPDIR}/${STEAMAPP}/addons/sourcemod/translations/"
 			cp -R "$TEMPDIR/surftimer/addons/sourcemod/gamedata/"* "${STEAMAPPDIR}/${STEAMAPP}/addons/sourcemod/gamedata/"
-			cp -R "$TEMPDIR/surftimer/addons/stripper/"* "${STEAMAPPDIR}/${STEAMAPP}/addons/"
+			cp -R "$TEMPDIR/surftimer/addons/stripper/" "${STEAMAPPDIR}/${STEAMAPP}/addons/"
 
 			cp -Rn "$TEMPDIR/surftimer/cfg/"* "${STEAMAPPDIR}/${STEAMAPP}/cfg/"
 			cp -R "$TEMPDIR/surftimer/maps/"* "${STEAMAPPDIR}/${STEAMAPP}/maps/"
